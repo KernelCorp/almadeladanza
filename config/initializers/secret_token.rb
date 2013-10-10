ed971620421e0437ac62d1472281b38b52cb6587
max-konin <mk@kernel-corp.com>
2013-10-10 17:44:00 +0700

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Almadeladanza::Application.config.secret_token = 'e2e631a88f7ae763452d0110d7ebe8e213829552b306b1136bc575ec91a645590cca6d019383f297aa4c841b768a1a3d6d71f1d6a7c6e6793271d75b80dd7d86'
