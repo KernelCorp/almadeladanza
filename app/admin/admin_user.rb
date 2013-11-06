e56e888a23b82132ee1da86bffe8228b14aaf09d
mitchenkod <mitchenko1897@mail.ru>
2013-11-06 14:59:22 +0700

    images are in progress

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register User do
+ActiveAdmin.register AdminUser do
   index do
     column :email
     column :current_sign_in_at
---
+++ app/models/coach.rb
@@ -0,0 +1,8 @@
+class Coach < User
+  attr_accessible :name, :biography, :avatar
+
+  has_attached_file :avatar,
+      :path => ":rails_root/public/coaches/:filename",
+      :url => "/coaches/:filename"
+
+end
