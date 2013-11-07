dea7c0d2c992c81b9d3ffad0657cf3844b730afa
Zamyatin-AA <stekbox@gmail.com>
2013-11-07 18:29:28 +0700

    Dance style seeding done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131105102646) do
+ActiveRecord::Schema.define(:version => 20131107110234) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20131105102646) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "type"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
