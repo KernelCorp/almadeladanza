c593585ba8f9237fcc2f2d86b5938038763c901d
Zamyatin-AA <stekbox@gmail.com>
2013-11-20 16:18:16 +0700

    Fixed gallery bug.

--- app/models/images/gallery_image.rb
+++ app/models/images/gallery_image.rb
@@ -1,11 +1,14 @@
 class GalleryImage < Image
+
   belongs_to :gallery_event
+
   has_attached_file :path,
                     :styles => {
                         :large => ["500x320#", :png],
                         :small => ["150x115#", :png],
                         :thumb => ["50x50#", :png]
                     },
-                    :path => ':rails_root/public/system/images/:style/:filename',
-                    :url => '/system/images/:style/:filename'
+                    :path => ':rails_root/public/system/gallery_images/:style/:filename',
+                    :url => '/system/gallery_images/:style/:filename'
+
 end
