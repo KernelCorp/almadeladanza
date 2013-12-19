467df3493ca968654174d1e42c064eef0771d015
max-konin <mk@kernel-corp.com>
2013-12-19 21:09:36 +0700

    update mailing

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,10 +1,11 @@
+#encoding: utf-8
 class ClientMailer < ActionMailer::Base
-  default from: "almadeladanza@web.com"
+  default from: "site@almadeladanza.ru"
 
   def client_reg_email(client)
     @client = client
-    mail to: 'truestek11@yandex.ru',
-         subject: 'New registration'
+    mail to: 'admin@almadeladanza.ru',
+         subject: 'Новый клиент'
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,16 @@ Almadeladanza::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.yandex.ru',
+      port:                  587,
+      domain:               'almadeladanza.ru',
+      user_name:            'site@almadeladanza.ru',
+      password:             'alma!@#alma',
+      authentication:       'plain',
+      enable_starttls_auto:  true  }
+
+
 end
