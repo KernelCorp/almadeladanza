ff70762d8ba18665e5c4f9b4b325b9874f5afe3a
max-konin <mk@kernel-corp.com>
2014-01-21 19:10:17 +0700

    f

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require bxslider
 //= require fancybox
 //= require jquery.ui.tabs
-//= require_tree ./lib/
+//= require_tree ./lib
 
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
