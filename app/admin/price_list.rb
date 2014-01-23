dc1076d3850b376007975076b27f4ff9657ee67d
mitchenkod <mitchenko1897@mail.ru>
2014-01-23 15:42:52 +0700

    price table is done

--- app/admin/price_list.rb
+++ app/admin/price_list.rb
@@ -1,4 +1,22 @@
 ActiveAdmin.register PriceList do
   config.filters = false
   actions :all, :except => [:destroy, :new, :create]
+
+  index do
+    column :id
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :content do |list|
+        raw list.content
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
 end
